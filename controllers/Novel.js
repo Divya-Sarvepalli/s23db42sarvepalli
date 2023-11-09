a705e8b21ebcbc51f71574953a6996bc3a26ab3b
Divya-Sarvepalli <143059445+Divya-Sarvepalli@users.noreply.github.com>
2023-11-08 19:32:18 -0600

    Updated Novel file

--- controllers/Novel.js
+++ controllers/Novel.js
@@ -1,8 +1,16 @@
 var Novel = require('../models/Novel');
 // List of all Novels
-exports.Novel_list = function(req, res) {
-res.send('NOT IMPLEMENTED: Novel list');
-};
+exports.Novel_list = async function(req, res) {
+    try{
+    theNovels = await Novel.find();
+    res.send(theNovels);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
+
 // for a specific Novel.
 exports.Novel_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: Novel detail: ' + req.params.id);

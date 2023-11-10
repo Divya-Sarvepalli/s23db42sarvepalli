146a676c4a73262ef0e668834ee35a4a3c777cb0
Divya-Sarvepalli <143059445+Divya-Sarvepalli@users.noreply.github.com>
2023-11-10 14:09:52 -0600

    Updated novel.js file

--- controllers/Novel.js
+++ controllers/Novel.js
@@ -12,9 +12,22 @@ exports.Novel_list = async function(req, res) {
     };
 
 // for a specific Novel.
-exports.Novel_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: Novel detail: ' + req.params.id);
+//exports.Novel_detail = function(req, res) {
+//res.send('NOT IMPLEMENTED: Novel detail: ' + req.params.id);
+//};
+
+// for a specific Novel.
+exports.Novel_detail = async function(req, res) {
+console.log("detail" + req.params.id)
+try {
+result = await Novel.findById( req.params.id)
+res.send(result)
+} catch (error) {
+res.status(500)
+res.send(`{"error": document for id ${req.params.id} not found`);
+}
 };
+
 // Handle Novel create on POST.
 exports.Novel_create_post = function(req, res) {
 res.send('NOT IMPLEMENTED: Novel create POST');
@@ -24,8 +37,25 @@ exports.Novel_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: Novel delete DELETE ' + req.params.id);
 };
 // Handle Novel update form on PUT.
-exports.Novel_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: Novel update PUT' + req.params.id);
+
+exports.Novel_update_put = async function(req, res) {
+console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+try {
+let toUpdate = await Novel.findById( req.params.id)
+// Do updates of properties
+if(req.body.Novel_type)
+toUpdate.Novel_type = req.body.Novel_type;
+if(req.body.size) toUpdate.size = req.body.size;
+if(req.body.cost) toUpdate.cost = req.body.cost;
+let result = await toUpdate.save();
+console.log("Sucess " + result)
+res.send(result)
+} catch (err) {
+res.status(500)
+res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+}
 };
 // VIEWS
 // Handle a show all view
